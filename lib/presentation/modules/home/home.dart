5f751d057892d56cbce997e0b23be2507104aa80
al azad <alazad214@gmail.com>
2024-07-30 15:43:34 +0600

    drawer added

--- lib/presentation/modules/home/home.dart
+++ lib/presentation/modules/home/home.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:universe_it_project/widgets/custom_drawer.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       body: CustomScrollView(
-        slivers: <Widget>[
+        slivers: [
           SliverAppBar(
             expandedHeight: 200.0,
             floating: false,
             pinned: true,
             flexibleSpace: FlexibleSpaceBar(
               title: Text('SliverAppBar Example'),
-              background: Image.network(
-                'https://example.com/image.jpg',
-                fit: BoxFit.cover,
-              ),
             ),
           ),
           SliverList(
             delegate: SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
+              (BuildContext context, int index) {
                 return ListTile(
                   leading: Icon(Icons.star),
                   title: Text('Item #$index'),
@@ -33,6 +30,7 @@ class Home extends StatelessWidget {
           ),
         ],
       ),
+      drawer: CustomDrawer(),
     );
   }
 }
--- lib/presentation/modules/splash/splash.dart
+++ lib/presentation/modules/splash/splash.dart
@@ -1,7 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:universe_it_project/presentation/modules/home/home.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
