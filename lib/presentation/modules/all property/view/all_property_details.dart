c8d9b9eb27e180c629aa99598d116bbd4fc78e70
al azad <alazad214@gmail.com>
2024-08-02 20:39:13 +0600

    all proppety added

---
+++ lib/presentation/modules/all property/view/all_property_details.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllPropertyDetails extends StatelessWidget {
+  const AllPropertyDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("All Property Details"),),
+    );
+  }
+}
---
+++ lib/utils/property_utils.dart
@@ -0,0 +1,32 @@
+final List<Map<String, dynamic>> propertyUtils = [
+  {
+    'img': "assets/images/flat.jpg",
+    'time': "May 16 2024",
+    'city': 'Modhubak city',
+    'location': 'AftabNogor, Badda ',
+    'features': '3b, 2b, 1g',
+    'price': "Call for Price"
+  },  {
+    'img': "assets/images/flat.jpg",
+    'time': "May 16 2024",
+    'city': 'Modhubak city',
+    'location': 'AftabNogor, Badda ',
+    'features': '3b, 2b, 1g',
+    'price': "Call for Price"
+  },  {
+    'img': "assets/images/flat.jpg",
+    'time': "May 16 2024",
+    'city': 'Modhubak city',
+    'location': 'AftabNogor, Badda ',
+    'features': '3b, 2b, 1g',
+    'price': "Call for Price"
+  },  {
+    'img': "assets/images/flat.jpg",
+    'time': "May 16 2024",
+    'city': 'Modhubak city',
+    'location': 'AftabNogor, Badda ',
+    'features': '3b, 2b, 1g',
+    'price': "Call for Price"
+  },
+
+];
