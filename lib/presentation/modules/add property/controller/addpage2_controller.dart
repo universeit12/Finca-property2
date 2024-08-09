68a367ea9ed10c127fdf6b04550088cf589a83f3
al azad <alazad214@gmail.com>
2024-08-09 22:10:25 +0600

    2nd page validation added

---
+++ lib/presentation/modules/add property/controller/addpage2_controller.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
+
+class Addpage2Controller extends GetxController {
+  final SingleValueDropDownController florController =
+      SingleValueDropDownController();
+  final TextEditingController titlecontroller = TextEditingController();
+  final TextEditingController descriptioncontroller = TextEditingController();
+  final TextEditingController mobilecontroller = TextEditingController();
+}
--- lib/presentation/modules/add property/view/addpage2.dart
+++ lib/presentation/modules/add property/view/addpage2.dart
@@ -1,60 +1,85 @@
+import 'package:dropdown_textfield/dropdown_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
 import 'package:universe_it_project/widgets/custom_button.dart';
 import 'package:universe_it_project/widgets/custom_text.dart';
-
+import '../../../../widgets/custom_dropdown.dart';
+import '../controller/addpage2_controller.dart';
 import '../widgets/custom_text_icon.dart';
 import '../widgets/textfield_custom.dart';
 
 class AddPage2 extends StatelessWidget {
   final PageController pageController;
 
-  const AddPage2({super.key, required this.pageController});
-
+  AddPage2({super.key, required this.pageController});
+  final controller = Get.put(Addpage2Controller());
+  final formkey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            IconButton(
-                onPressed: () {
-                  pageController.previousPage(
-                      duration: const Duration(milliseconds: 300),
-                      curve: Curves.easeInOut);
+        child: Form(
+          key: formkey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IconButton(
+                  onPressed: () {
+                    pageController.previousPage(
+                        duration: const Duration(milliseconds: 300),
+                        curve: Curves.easeInOut);
+                  },
+                  icon: const Icon(Icons.arrow_back_ios)),
+              const CustomText(text: "Property additional information"),
+              const CustomTextIcon(text: "Total number of floor"),
+              DropdownWidget(
+                dropDownList: [
+                  DropDownValueModel(name: '1', value: "1"),
+                  DropDownValueModel(name: '2', value: "2"),
+                  DropDownValueModel(name: '3', value: "3"),
+                  DropDownValueModel(name: '4', value: "4"),
+                  DropDownValueModel(name: '5', value: "5"),
+                  DropDownValueModel(name: '6', value: "6"),
+                  DropDownValueModel(name: '7', value: "7"),
+                  DropDownValueModel(name: '8', value: "8"),
+                ],
+                hintText: "Select an item",
+                enablesearch: false,
+                controller: controller.florController,
+              ),
+              const CustomTextIcon(text: "Title"),
+              ReusableTextField(
+                hintText:
+                    '212 sqft, 1 Bed Almost Ready Apartment/Flats for Sale at Karatia',
+                maxLines: 2,
+                controller: controller.titlecontroller,
+              ),
+              const CustomTextIcon(text: "Description"),
+              ReusableTextField(
+                hintText: 'Enter Description',
+                maxLines: 4,
+                controller: controller.descriptioncontroller,
+              ),
+              const CustomTextIcon(text: "Phone Number"),
+              ReusableTextField(
+                hintText: '+8801*********',
+                keyboardtype: TextInputType.phone,
+                suffixIcon: Icon(Icons.arrow_drop_down),
+                controller: controller.mobilecontroller,
+              ),
+              const SizedBox(height: 10.0),
+              CustomButton(
+                text: "Post",
+                ontap: () {
+                  if (formkey.currentState!.validate()) {
+                    Fluttertoast.showToast(msg: "Post Done");
+                  }
                 },
-                icon: const Icon(Icons.arrow_back_ios)),
-            const CustomText(text: "Property additional information"),
-            const CustomTextIcon(text: "Total number of floor"),
-            const ReusableTextField(
-              hintText: '1',
-              readOnly: true,
-              suffixIcon: Icon(Icons.arrow_drop_down),
-            ),
-            const CustomTextIcon(text: "Title"),
-            const ReusableTextField(
-              hintText:
-                  '212 sqft, 1 Bed Almost Ready Apartment/Flats for Sale at Karatia',
-              maxLines: 2,
-            ),
-            const CustomTextIcon(text: "Description"),
-            const ReusableTextField(
-              hintText: 'Enter Description',
-              maxLines: 4,
-            ),
-            const CustomTextIcon(text: "Phone Number"),
-            const ReusableTextField(
-              hintText: '+8801*********',
-              keyboardtype: TextInputType.phone,
-              suffixIcon: Icon(Icons.arrow_drop_down),
-            ),
-            const SizedBox(height: 10.0),
-            CustomButton(
-              text: "Post",
-              ontap: () {},
-            ),
-          ],
+              ),
+            ],
+          ),
         ),
       ),
     );
