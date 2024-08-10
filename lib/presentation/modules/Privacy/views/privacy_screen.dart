a14a34163e5686322b380f2e2ddd9d1c312a4f0d
al azad <alazad214@gmail.com>
2024-08-10 16:36:46 +0600

    privacy page modified

--- lib/presentation/modules/Privacy/views/privacy_screen.dart
+++ lib/presentation/modules/Privacy/views/privacy_screen.dart
@@ -8,18 +8,25 @@ class PrivacyScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Privacy"),
+        title: const Text("Privacy Policy"),
+        titleSpacing: 0,
         backgroundColor: AppColor.baseColor,
       ),
-      body: Padding(
+      body: const Padding(
         padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 15.0),
         child: SingleChildScrollView(
-          child: Text(
-              "Welcome to [Your App Name]. We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our mobile application.\n"
-              "Personal Data: We may collect personally identifiable information such as your name, email address, and phone number.\n"
-              "Usage Data: We collect information about how you use our application, including device information (e.g., IP address, operating system) and application usage details (e.g., pages visited, features used).\n"
-              "Location Data: If you enable location services, we may collect information about your location."),
-        ),
+            child: Text.rich(TextSpan(children: [
+          TextSpan(
+              text: "Welcome",
+              style: TextStyle(fontSize: 25.0, color: Colors.teal)),
+          TextSpan(
+              text:
+                  " to Finca Properties. We are committed to protecting your privacy. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you use our mobile application.\n"
+                  "Personal Data: We may collect personally identifiable information such as your name, email address, and phone number.\n"
+                  "Usage Data: We collect information about how you use our application, including device information (e.g., IP address, operating system) and application usage details (e.g., pages visited, features used).\n"
+                  "Location Data: If you enable location services, we may collect information about your location.",
+              style: TextStyle(fontSize: 16.0))
+        ]))),
       ),
     );
   }
